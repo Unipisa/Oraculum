71b343c58a84c8d912e2841c239780fbcefce14d
DAMIEN CIAGOLA <d.ciagola@cineca.it>
2023-10-26 12:33:00 +0200

    create structure API Eval

--- OraculumEvaluate/EvalAPI/AugmentedAPI.py
+++ OraculumEvaluate/EvalAPI/AugmentedAPI.py
@@ -1,2 +1,13 @@
-import flask
+# Prova di Utilizzo augmented_data + Eval_ragas
+
+
+
+from OraculumEvaluate.augmend_data import augment_json_data
+from OraculumEvaluate.Eval_ragas import ragasEvaluate
+import json
+
+# Load Json
+json_query = json.load(open("../polimiEval1.json"))
+print(json_query)
+json_augmented = augment_json_data(json_query)
 
--- OraculumEvaluate/Eval_ragas.py
+++ OraculumEvaluate/Eval_ragas.py
@@ -1,51 +1,84 @@
 import os
 # os.environ["OPENAI_API_KEY"] = "sk-"
 from ragas import evaluate
-from ragas.metrics import faithfulness, answer_relevancy, context_recall , context_precision
+from ragas.metrics import faithfulness, answer_relevancy, context_recall, context_precision
 from datasets import load_dataset
+import numpy as np
+from statistics import harmonic_mean
 import json
-import pandas as pd
 
-filename = 'polimiEval1_preprocessed'
-path = f'{filename}.json'
-mydataset=load_dataset("json", data_files=path)
+def corrected_harmonic_mean(data):
+    # Filtering out zero values
+    non_zero_data = [x for x in data if x != 0]
 
-metrics_t = [faithfulness, answer_relevancy, context_recall, context_precision]
+    # If all values are zero, return 0
+    if len(non_zero_data) == 0:
+        return 0
+
+    # Calculate harmonic mean of the non-zero values
+    hm = harmonic_mean(non_zero_data)
+
+    # Apply corrective factor
+    corrected_hm = hm * (len(non_zero_data) / len(data))
+
+    return corrected_hm
+def ragasEvaluate(file_json_input, metrics_t=None):
+    # fileJsonInput structure:
+    #[
+    #    {
+    #        "question": "",
+    #        "ground_truths": [
+    #           ""],
+    #        "contexts": [
+    #           "","",""],
+    #        "answer": ""
+    #    }
+    #]
+
+    # Imposta le metriche predefinite se metrics_t è None
+    if metrics_t is None:
+        metrics_t = {
+            'faithfulness': faithfulness,
+            'answer_relevancy': answer_relevancy,
+            'context_recall': context_recall,
+            'context_precision': context_precision
+        }
+
+    with open("file_json_input.json", "w") as outfile: #TODO Evitare la creazione di un file temporaneo!
+        json.dump(file_json_input, outfile)
+
+    mydataset = load_dataset("json", data_files="file_json_input.json")
+
+    augmented_data = []
+    metric_values = {metric_name: [] for metric_name in metrics_t}
+
+    total_elements = len(mydataset['train'])
+    for index, row in enumerate(mydataset['train']):
+        print(f"Evaluating {index + 1} of {total_elements}")
+        filtered_dataset = mydataset.filter(lambda x: x == row)
+        result = evaluate(filtered_dataset['train'], metrics=list(metrics_t.values()))
+
+        for metric_name, metric_function in metrics_t.items():
+            metric_values[metric_name].append(result[metric_name])
+
+        row['evaluation'] = result
+        augmented_data.append(row)
+
+    mean_metric_values = {metric_name: np.mean(values) for metric_name, values in metric_values.items()}
+    ragas_score = corrected_harmonic_mean(list(mean_metric_values.values()))
+
+    final_row = {'ragas_score': ragas_score, **mean_metric_values}
+    augmented_data.append(final_row)
+
+    if os.path.exists("file_json_input.json"): #TODO ELIMINARE QUESTO GESTENDO INPUT SU DATA (TODO SOPRA)
+        os.remove("file_json_input.json")
+
+    return augmented_data
 
 
-augmented_data = []
+# Esempio di utilizzo con metriche custom
+#selected_metrics = {
+#    'faithfulness': faithfulness,
+#    'answer_relevancy': answer_relevancy
+#}
 
-total_elements = len(mydataset['train'])
-for index, row in enumerate(mydataset['train']):
-    print(f"Evaluating {index + 1} of {total_elements}")
-    
-    filtered_dataset = mydataset.filter(lambda x: x == row)
-    
-    result = evaluate(filtered_dataset['train'], metrics=metrics_t)
-    print(result)
-
-    row['evaluation'] = result
-    augmented_data.append(row)
-
-print('Evaluating dataset')
-#* Calcolare results senza rifare evaluate di tutto il dataset
-# results = evaluate(mydataset['train'], metrics=metrics_t)
-# print(results)
-# augmented_data.append({"ground_truths": "Dataset evaluation", "evaluation": results})
-
-with open("./OraculumEvaluate/augmented_polimiEval1_preprocessed.json", "w", encoding='UTF-8') as outfile:
-    json.dump(augmented_data, outfile)
-    df = pd.json_normalize(augmented_data)
-    df.to_excel(f'./OraculumEvaluate/{filename}.xlsx', index=False, engine='openpyxl')
-
-
-# if len(values) > 1:
-#             reciprocal_sum = np.sum(1.0 / np.array(values))  # type: ignore
-#             self["ragas_score"] = len(values) / reciprocal_sum
-
-
-
-# Valutazione di 13 domande aggiornato al 23/10/2023
-
-# {'ragas_score': 0.3916, 'faithfulness': 0.7742, 'answer_relevancy': 0.8751, 
-# 'context_recall': 0.2718, 'answer_similarity': 0.8462, 'context_precision': 0.1828}
