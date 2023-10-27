6987b269f7ef4211807bfa2c89a902862dc89540
ALESSANDRO SITTA <a.sitta@cineca.it>
2023-10-27 16:20:15 +0200

    setup pr api

---
+++ OraculumApi/Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Text.Json.Serialization;
+using Oraculum;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers()
+.AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
+builder.Services.AddDistributedMemoryCache();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddApiVersioning(x =>
+{
+    x.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.ReportApiVersions = true;
+    x.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader());
+});
+
+builder.Services.AddSession(builder =>
+{
+    builder.IdleTimeout = TimeSpan.FromMinutes(20);
+    builder.Cookie.HttpOnly = true;
+    builder.Cookie.IsEssential = true;
+});
+
+builder.Services.AddVersionedApiExplorer(s =>
+{
+    s.GroupNameFormat = "'v'VVV";
+    s.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var _configuration = builder.Configuration;
+var _env = builder.Environment;
+builder.Services.AddSingleton<SibyllaManager>(new SibyllaManager(new Oraculum.Configuration()
+{
+    WeaviateEndpoint = _configuration["Weaviate:ServiceEndpoint"],
+    WeaviateApiKey = _configuration["Weaviate:ApiKey"],
+    OpenAIApiKey = _configuration["OpenAI:ApiKey"],
+    OpenAIOrgId = _configuration["OpenAI:OrgId"]
+}, Path.Combine(_env.ContentRootPath, "SibyllaeConf")));
+
+var app = builder.Build();
+
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+ {
+     foreach (var description in provider.ApiVersionDescriptions)
+     {
+         options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+     }
+ });
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.MapControllers();
+
+app.Run();
--- src/Oraculum/SibyllaManager.cs
+++ src/Oraculum/SibyllaManager.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 
 namespace Oraculum
 {
@@ -14,6 +18,7 @@ namespace Oraculum
         private string _dataDir;
         private Configuration _oraculumConf;
         private ILogger _logger;
+        private Oraculum _oraculum;
 
         public SibyllaManager(Configuration oraculumConf, string configurationsPath, ILogger? logger = null)
         {
@@ -24,6 +29,7 @@ namespace Oraculum
 
             _dataDir = configurationsPath;
             _oraculumConf = oraculumConf;
+            _oraculum = new Oraculum(oraculumConf);
         }
 
         private string ConfFile(string name)
@@ -68,7 +74,6 @@ namespace Oraculum
             }
         }
 
-
         public async Task<(Guid, Sibylla)> AddSibylla(string name, Configuration? oraculumConf = null, DateTime? expiration = null, bool connect = true)
         {
             var conf = SibyllaConf.FromJson(File.ReadAllText(ConfFile(name)));
@@ -79,6 +84,8 @@ namespace Oraculum
                 await s.Connect();
             return (id, s);
         }
+
+
 
         public void CollectExpiredSibyllae()
         {
@@ -92,6 +99,191 @@ namespace Oraculum
 
             foreach (var key in toremove)
                 _sibyllae.Remove(key);
+        }
+
+        public async Task<Fact?> GetFactById(Guid id)
+        {
+            // check Oraculum connection
+            if (!_oraculum.IsConnected)
+                await _oraculum.Connect();
+            //try catch keynotfound, return null
+            try
+            {
+                Fact? fact = await _oraculum.GetFact(id) ?? throw new KeyNotFoundException($"Fact with id {id} not found.");
+                return fact;
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        //delete fact by id
+        public async Task DeleteFactById(Guid id)
+        {
+            // check Oraculum connection
+            if (!_oraculum.IsConnected)
+                await _oraculum.Connect();
+            //try catch keynotfound, return null
+            try
+            {
+                await _oraculum.DeleteFact(id);
+            }
+            catch (KeyNotFoundException)
+            {
+                return;
+            }
+        }
+
+        // find relevant facts
+        public async Task<ICollection<Fact>> FindRelevantFacts(string query, float? distance = null, int? limit = null, int? autoCut = null, string[]? factTypeFilter = null, string[]? categoryFilter = null, string[]? tagsFilter = null)
+        {
+            // check Oraculum connection
+            if (!_oraculum.IsConnected)
+                await _oraculum.Connect();
+            if (string.IsNullOrEmpty(query))
+            {
+                return new List<Fact>();
+            }
+
+            var factFilter = new FactFilter()
+            {
+                Limit = limit,
+                Distance = distance,
+                Autocut = autoCut,
+                FactTypeFilter = factTypeFilter,
+                CategoryFilter = categoryFilter,
+                TagsFilter = tagsFilter
+            };
+
+            var facts = await _oraculum.FindRelevantFacts(query, factFilter);
+            return facts;
+        }
+
+        // Get all facts
+        public async Task<ICollection<Fact>> GetAllFacts(int limit = 0, int offset = 0, string? sort = null, string? order = null)
+        {
+            // check Oraculum connection
+            if (!_oraculum.IsConnected)
+                await _oraculum.Connect();
+
+            var facts = await _oraculum.ListFacts(limit: limit, offset: offset, sort: sort, order: order);
+
+            return facts;
+        }
+
+        // Add List of facts
+        public async Task<int> AddFacts(ICollection<Fact> facts)
+        {
+            // check Oraculum connection
+            if (!_oraculum.IsConnected)
+                await _oraculum.Connect();
+
+            var newFacts = await _oraculum.AddFact(facts);
+
+            return newFacts;
+        }
+
+        // get all Sibyllae Configurations
+        public Task<List<SibyllaConf>> GetSibyllae()
+        {
+            var sibyllaeConfigs = new List<SibyllaConf>();
+            DirectoryInfo di = new DirectoryInfo(_dataDir);
+            FileInfo[] confFiles = di.GetFiles("*.json");
+            if (confFiles.Length != 0)
+            {
+                foreach (FileInfo fi in confFiles)
+                {
+                    var name = Path.GetFileNameWithoutExtension(fi.Name);
+                    try
+                    {
+                        var s = SibyllaConf.FromJson(File.ReadAllText(ConfFile(name))) ?? throw new Exception($"Configuration file {ConfFile(name)} is not valid.");
+                        // TODO: this manages the Id
+                        s.Title = name;
+                        sibyllaeConfigs.Add(s);
+                    }
+                    catch (System.Text.Json.JsonException)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return Task.FromResult(sibyllaeConfigs);
+        }
+
+        // get a Sibylla configuration by Id
+        public async Task<SibyllaConf> GetSibyllaById(String sibyllaId)
+        {
+            var sibyllae = await Task.Run(() => GetSibyllae());
+            return sibyllae.FirstOrDefault(f => f.Title != null && f.Title.Equals(sibyllaId));
+        }
+
+        // delete a Sibylla by Id
+        public async Task<Boolean> DeleteSibyllaById(String sibyllaId)
+        {
+            var sibyllae = await Task.Run(() => GetSibyllae());
+            var s = sibyllae.FirstOrDefault(f => f.Title != null && f.Title.Equals(sibyllaId));
+            if (s != null)
+            {
+                // build the right path for the file
+                var sepChar = Path.DirectorySeparatorChar.ToString();
+                var altChar = Path.AltDirectorySeparatorChar.ToString();
+                var path = _dataDir;
+                if (!path.EndsWith(sepChar) && !path.EndsWith(altChar))
+                {
+                    path += sepChar;
+                }
+                path += s.Title;
+                path += ".json";
+                // if the file doesn't exist in the case of an Id different than the Title attribute
+                if (!File.Exists(path))
+                {
+                    return false;
+                }
+                File.Delete(path);
+                return true;
+            }
+            return false;
+        }
+
+        // save a Sibylla configuration by Id
+        public async Task<Boolean> SaveSibylla(SibyllaConf sibyllaConf)
+        {
+            return await SaveOrUpdateSibyllaConfigFile(sibyllaConf, false);
+        }
+
+        // update the configuration of a Sibylla
+        public async Task<Boolean> UpdateSibylla(SibyllaConf sibyllaConf)
+        {
+            return await SaveOrUpdateSibyllaConfigFile(sibyllaConf, true); ;
+        }
+
+        private async Task<Boolean> SaveOrUpdateSibyllaConfigFile(SibyllaConf sibyllaConf, Boolean update)
+        {
+            // remeber that the title attribute must be unique as is being used as an Id or file name in this case
+            if (sibyllaConf.Title == null)
+            {
+                return false;
+            }
+            // build the right path for the file
+            var sepChar = Path.DirectorySeparatorChar.ToString();
+            var altChar = Path.AltDirectorySeparatorChar.ToString();
+            var path = _dataDir;
+            if (!path.EndsWith(sepChar) && !path.EndsWith(altChar))
+            {
+                path += sepChar;
+            }
+            path += sibyllaConf.Title;
+            path += ".json";
+            if (File.Exists(path) && !update)
+            {
+                // the file already exist
+                throw new IOException("The configuration file already exist");
+            }
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            await using FileStream createStream = File.Create(path);
+            await JsonSerializer.SerializeAsync(createStream, sibyllaConf, options);
+            return true;
         }
 
         public void RemoveSibylla(string name, Guid id)
