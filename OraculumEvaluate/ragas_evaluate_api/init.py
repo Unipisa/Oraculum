10d862b59f11bd9de29549d8df840396a5b8bbb7
ALESSANDRO SITTA <a.sitta@cineca.it>
2023-11-28 14:56:14 +0100

    rimosso oraculum evaluate

--- OraculumEvaluate/ragas_evaluate_api/init.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from models import db, Output
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ragas_evaluate.db'
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
