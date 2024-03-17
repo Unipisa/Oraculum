9065c7e3141353409318324279e26d69d37f656e
Antonio Cisternino <antonio.cisternino@unipi.it>
2024-03-17 12:44:22 +0100

    Abstracted in the Weaviate provider all the business logic. Now Oraculum only invoke the default provider.

---
+++ src/Oraculum/IFactProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oraculum;
+
+public class FactFilter
+{
+    public int? Limit = null;
+    public float? Distance = null;
+    public int? Autocut = null;
+    public float? AutocutPercentage = null;
+    public string[]? FactTypeFilter = null;
+    public string[]? CategoryFilter = null;
+    public string[]? TagsFilter = null;
+    public DateTime? AddedSince = null;
+}
+
+public interface IFactProvider
+{
+    public Task<ICollection<Fact>> FindRelevantFacts(string concept, FactFilter? factFilter = null);
+}
